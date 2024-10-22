9de58f4b801d57e1f8a37dca7118239568a6aef2
brenobarroso <barrososbreno@icloud.com>
2024-10-21 21:36:10 -0300

    add Person class and update RentDbContext

--- RentManagerAPI/Database/RentDbContext.cs
+++ RentManagerAPI/Database/RentDbContext.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using SeuNamespace.Models;
 
 namespace Database;
 
 public class RentDbContext : DbContext
 {
-    public RentDbContext(DbContextOptions<RentDbContext> options) :base(options)
+    public RentDbContext(DbContextOptions<RentDbContext> options) : base(options)
     {
-        
     }
+
+    public DbSet<Person> Persons { get; set; }
 }
---
+++ RentManagerAPI/Models/Persons/Person.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SeuNamespace.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
