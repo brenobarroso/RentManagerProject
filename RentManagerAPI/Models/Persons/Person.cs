ae67916d4bf9e42ed761768085a6ba890e2719f9
brenobarroso <barrososbreno@icloud.com>
2024-10-22 07:44:05 -0300

    add data annotations and add enum Role, all for Person model

--- RentManagerAPI/Models/Persons/Person.cs
+++ RentManagerAPI/Models/Persons/Person.cs
@@ -1,16 +1,44 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace SeuNamespace.Models
 {
     public class Person
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string? LastName { get; set; }
+
+        [EmailAddress]
+        [StringLength(200)]
         public string? Email { get; set; }
+
+        [Phone]
+        [StringLength(11)]
         public string? PhoneNumber { get; set; }
+
+        [StringLength(300)]
         public string? Address { get; set; }
-        public DateTime UpdateAt { get; set; }
-        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? UpdateAt { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+    }
+
+    public enum Role
+    {
+        Lessor, // Locador
+        Lessee  // Locatário
     }
 }
