ffa62b72cfd1514a18f4db98e76a4aa0bb31cf51
brenobarroso <barrososbreno@icloud.com>
2024-10-22 15:03:08 -0300

    explicit relationship between Property and Person on DbContext

--- RentManagerAPI/Database/RentDbContext.cs
+++ RentManagerAPI/Database/RentDbContext.cs
@@ -2,14 +2,27 @@ using Microsoft.EntityFrameworkCore;
 using Models.Persons;
 using Models.Properties;
 
-namespace Database;
-
-public class RentDbContext : DbContext
+public class RentManagerDbContext : DbContext
 {
-    public RentDbContext(DbContextOptions<RentDbContext> options) : base(options)
-    {
-    }
-
-    public DbSet<Person> Persons { get; set; }
+    public DbSet<Person> People { get; set; }
     public DbSet<Property> Properties { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configuração do relacionamento Lessor (Locador) com Property
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.OwnedProperties)
+            .WithOne(p => p.Lessor)
+            .HasForeignKey(p => p.LessorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Configuração do relacionamento entre Lessee (Locatário) e Property
+        modelBuilder.Entity<Property>()
+            .HasOne(p => p.CurrentLessee)
+            .WithOne()
+            .HasForeignKey<Property>(p => p.CurrentLesseeId)
+            .OnDelete(DeleteBehavior.SetNull); // Define que o locatário pode ser removido sem apagar o imóvel
+    }
 }
